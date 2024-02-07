829ff851ae12ee8ac21878ab7aca4717eede925e
Kelly Cruz <30739866+kellypc@users.noreply.github.com>
2024-02-07 16:38:38 -0300

    Consume the github repositories api

---
+++ javascript/main.js
@@ -0,0 +1,34 @@
+const ul = document.querySelector('ul');
+
+function getApiGitHub(){
+  fetch('https://api.github.com/users/kellypc/repos')
+    .then(async res => {
+      if (!res.ok) {
+        throw new Error(res.status)
+      }
+
+      var data = await res.json()
+
+      data.map(item => {
+        console.log(item)
+        let li = document.createElement('li')
+        
+        li.innerHTML = `
+        <p>
+          <a href=${item.git_url}>${item.name.toUpperCase()}</a>
+        </p>
+        <p>Data de Criação:
+        ${
+          Intl.DateTimeFormat('pt-BR')
+          .format(new Date(item.created_at))
+        }
+        </p>
+        `
+        ul.appendChild(li);
+
+      })
+
+    }).catch(e => console.log(e))
+}
+
+getApiGitHub();
